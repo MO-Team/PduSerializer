f4419f501c0a42172c836daa30e7e0197784e1ab
Eyal Luxenburg <eluxenburg@gmail.com>
2013-07-07 21:45:44 +0300

    Add license

---
+++ PduSerializer/Serializers/EnumSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace PduSerializer.Serializers
+{
+    internal class EnumSerializer : ICustomSerializer
+    {
+        #region ISerializer Members
+
+        public void Serialize(object obj, BinaryWriter stream, ISerializationContext context)
+        {
+            var intObj = (Int32) obj;
+            if (context.ChangeByteOrder)
+                stream.Write(IPAddress.HostToNetworkOrder(intObj));
+            else
+                stream.Write(intObj);
+        }
+
+        public object Deserialize(BinaryReader stream, ISerializationContext context)
+        {
+            if (context.ChangeByteOrder)
+                return IPAddress.NetworkToHostOrder(stream.ReadInt32());
+            return stream.ReadInt32();
+        }
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ PduSerializer
 =============
 
 A fast and easy to use byte serializer 
+
+Licenses
+--------
+
+This software is distributed under the terms of the Free Software Foundation [Lesser GNU Public License (LGPL), version 2.1][D1] (see lgpl.txt).
+
+[D1]: http://www.gnu.org/licenses/lgpl-2.1-standalone.html
